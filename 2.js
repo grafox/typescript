312395388406b6478df7e14ca468d893c2bc3e0b
Gassan <g_fox_fts@yahoo.com>
2020-06-11 16:20:23 +0200

    shorthand object creation

--- 2.js
+++ 2.js
@@ -1,3 +1,4 @@
+// shorthand object creation
 function buildPersonData(_a) {
     var firstname = _a.firstname, lastname = _a.lastname, address = _a.address;
     return firstname + " \n            " + lastname + "\n            " + address;
