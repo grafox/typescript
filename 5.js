12abfd2cf598940be0a43a7ddebca7308c4d7d27
Gassan <g_fox_fts@yahoo.com>
2020-06-15 00:12:09 +0200

    enum

---
+++ 5.js
@@ -0,0 +1,4 @@
+var person = ['Gassan', 'Maha', 'Ali'];
+var twoPerson = ['Gassan', 'Maha'];
+var counters = [0, 1, 2];
+var tuple = ['Gas', 5];
--- 5.ts
+++
@@ -1,8 +0,0 @@
-let person:string[] = ['Gassan','Maha','Ali'];
-let twoPerson:string[] = ['Gassan','Maha'];
-
-let counters:number[]=[0,1,2];
-
-type Tuple = [string,number];
-
-let tuple: Tuple = ['Gas',5]
