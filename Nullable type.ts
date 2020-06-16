5dc8889670b90f795d55c14fbe4bf5b8f81f04f6
Gassan <g_fox_fts@yahoo.com>
2020-06-16 23:16:20 +0200

    Nullable type

---
+++ Nullable type.ts
@@ -0,0 +1,11 @@
+// Nullable type
+
+enum PlayerPosition2{
+    Guard,
+    Forward,
+    Center
+}
+interface Player2{
+    name: string,
+    position: PlayerPosition
+}
---
+++ Nullable_type.js
@@ -0,0 +1,10 @@
+// Nullable type
+var PlayerPosition2;
+(function (PlayerPosition2) {
+    PlayerPosition2[PlayerPosition2["Guard"] = 0] = "Guard";
+    PlayerPosition2[PlayerPosition2["Forward"] = 1] = "Forward";
+    PlayerPosition2[PlayerPosition2["Center"] = 2] = "Center";
+})(PlayerPosition2 || (PlayerPosition2 = {}));
+var Kobe2 = null | undefined, _a = void 0, _b = _a.name,  = _b === void 0 ? 'Kobe2' : _b, PlayerPosition2 = _a.position, Guard = _a.Guard;
+// "strictNullChecks": true in tsconfig.json
+Kobe2 = null;
