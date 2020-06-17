af7c024c5e41063f584f5b6b4c27f2f8aad2cc7f
Gassan <g_fox_fts@yahoo.com>
2020-06-17 02:33:42 +0200

    import

---
+++ importLodash.js
@@ -0,0 +1,5 @@
+//import {first} from 'lodash';
+var _ = require('lodash');
+var colors = ["Red", "Blue", "Green"];
+var firstColor = _.first(colors);
+console.log(firstColor);
---
+++ importLodash.ts
@@ -0,0 +1,7 @@
+//import {first} from 'lodash';
+import _ = require('lodash');
+const colors = ["Red","Blue","Green"];
+
+const firstColor = _.first(colors);
+
+console.log(firstColor);
